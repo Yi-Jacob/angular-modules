8bde6610c574bdb98f6b0b4ede7d38ed5b9e4929
Yi-Jacob <jakeyi00@gmail.com>
2023-01-02 17:00:09 -0800

    added accordion

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    {title: 'Manju', content: 'Rising Star'},
+    {title: 'Daddy', content: 'Icon'},
+     {title: 'r', content: 'r'}
+  ]
 
   constructor() {}
 
